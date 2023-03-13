3978e940ded05c0e0ba05995bb43b790b36af307
culcomber <m13076185140@gmail.com>
2023-03-14 00:27:15 +0800

    sync

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const cors = require('cors')
+require('dotenv').config()
+
+const Note = require('./models/note')
 
 const requestLogger = (request, response, next) => { // 打印出发送到服务器的每个请求的信息
   console.log('Method:', request.method)
@@ -20,7 +23,7 @@ app.use(express.json()) // 将请求的 JSON 数据转化为 JavaScript 对象
 app.use(requestLogger)
 app.use(express.static('build')) // 每当express收到一个HTTP GET请求时，它将首先检查build目录中是否包含一个与请求地址相对应的文件。
 
-let notes = [
+/*let notes = [
   {
     id: 1,
     content: "HTML is easy",
@@ -39,63 +42,78 @@ let notes = [
     date: "2022-05-30T19:20:14.298Z",
     important: true,
   },
-];
+];*/
 
 // request包含 HTTP 请求的所有信息，response定义如何对请求进行响应
-app.get("/", (request, response) => {
-  // Express 自动将 Content-Type 头的值设置为 text/html。响应的状态代码默认为 200
-  response.send("<h1>Hello World!</h1>");
-});
+// app.get('/', (request, response) => {
+//   // Express 自动将 Content-Type 头的值设置为 text/html。响应的状态代码默认为 200
+//   response.send("<h1>Hello World!</h1>");
+// });
 
-app.get("/api/notes", (request, response) => {
+app.get('/api/notes', (request, response) => {
   // Express 自动将 Content-Type 头设置为 application/json 的适当值
-  response.json(notes);
-});
-
-// Fetching a single resource
-app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-  if (note) {
-    response.json(note);
-  } else {
-    // 使用 status 方法来设置状态，并使用 end 方法来响应请求，而不发送任何数据。
-    response.status(404).end();
-  }
+  // response.json(notes);
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 });
 
 // Receiving data
-const generateId = () => {
+/*const generateId = () => {
   const maxId = notes.length > 0
     ? Math.max(...notes.map(n => n.id)) // 找到当前最大id
     : 0
   return maxId + 1
-}
+}*/
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
-  const note = {
+
+  /*const note = {
     content: body.content,
     important: body.important || false,
     date: new Date(),
     id: generateId(),
   }
   notes = notes.concat(note)
-  response.json(note)
+  response.json(note)*/
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
+// Fetching a single resource
+app.get('/api/notes/:id', (request, response) => {
+  /*const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
+  if (note) {
+    response.json(note);
+  } else {
+    // 使用 status 方法来设置状态，并使用 end 方法来响应请求，而不发送任何数据。
+    response.status(404).end();
+  }*/
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  })
+});
+
 // Deleting resources
-app.delete('/api/notes/:id', (request, response) => {
+/*app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
   notes = notes.filter(note => note.id !== id)
 
   response.status(204).end()
-})
+})*/
 
 app.use(unknownEndpoint) // 未定义路由
 
