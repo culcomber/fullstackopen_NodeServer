0b94220524eb0c85eaaebac622b7df65296cb32f
culcomber <m13076185140@gmail.com>
2023-03-15 17:26:56 +0800

    part4-4测试删除和新增，封装测试模块

--- controllers/notes.js
+++ controllers/notes.js
@@ -7,8 +7,8 @@ notesRouter.get('/', async (request, response) => {
 })
 
 // app.js app.use('/api/notes', notesRouter)
-notesRouter.get('/:id', (request, response, next) => {
-  Note.findById(request.params.id)
+notesRouter.get('/:id', async (request, response) => {
+  /*Note.findById(request.params.id)
     .then(note => {
       if (note) {
         response.json(note)
@@ -16,10 +16,27 @@ notesRouter.get('/:id', (request, response, next) => {
         response.status(404).end()
       }
     })
-    .catch(error => next(error))
+    .catch(error => next(error))*/
+  /*try {
+    const note = await Note.findById(request.params.id)
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
+  } catch(exception) {
+    next(exception)
+  }*/
+  // 在async路由中发生异常，执行会自动传递给错误处理中间件
+  const note = await Note.findById(request.params.id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
 })
 
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response) => {
   const body = request.body
 
   const note = new Note({
@@ -28,19 +45,23 @@ notesRouter.post('/', (request, response, next) => {
     date: new Date()
   })
 
-  note.save()
+  /*  note.save()
     .then(savedNote => {
-      response.json(savedNote)
+      response.status(201).json(savedNote)
     })
-    .catch(error => next(error))
+    .catch(error => next(error))*/
+  const savedNote = await note.save()
+  response.status(201).json(savedNote)
 })
 
-notesRouter.delete('/:id', (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
+notesRouter.delete('/:id', async (request, response) => {
+  /*  Note.findByIdAndRemove(request.params.id)
     .then(() => {
       response.status(204).end()
     })
-    .catch(error => next(error))
+    .catch(error => next(error))*/
+  await Note.findByIdAndRemove(request.params.id)
+  response.status(204).end()
 })
 
 notesRouter.put('/:id', (request, response, next) => {
--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const helper = require('./test_helper')
 const app = require('../app')
-
 const api = supertest(app) // 从app.js模块中导入Express应用，并将其与supertest函数包装成一个所谓的superagent对象
+const Note = require('../models/note')
 
-// 初始化数据库
+/*初始化数据库
 const Note = require('../models/note')
 const initialNotes = [
   {
@@ -17,16 +18,18 @@ const initialNotes = [
     date: new Date(),
     important: true,
   },
-]
+]*/
 beforeEach(async () => {
   await Note.deleteMany({})
-  let noteObject = new Note(initialNotes[0])
+  // let noteObject = new Note(initialNotes[0])
+  let noteObject = new Note(helper.initialNotes[0])
   await noteObject.save()
-  noteObject = new Note(initialNotes[1])
+  // noteObject = new Note(initialNotes[1])
+  noteObject = new Note(helper.initialNotes[1])
   await noteObject.save()
 })
 
-
+// --------------------------查询get('/api/notes')------------------------
 // api对象的方法调用前面有await关键字
 // Jest默认的5000ms的测试超时，第三个参数将超时设置为100000毫秒。确保测试不会因为运行时间而失败
 test('notes are returned as json', async () => {
@@ -36,19 +39,89 @@ test('notes are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 }, 100000)
 
-test('there are two notes', async () => {
+test('all notes are returned', async () => {
   const response = await api.get('/api/notes')
-  //   expect(response.body).toHaveLength(2)
-  expect(response.body).toHaveLength(initialNotes.length)
+
+  /*    expect(response.body).toHaveLength(2)
+  expect(response.body).toHaveLength(initialNotes.length)*/
+  expect(response.body).toHaveLength(helper.initialNotes.length)
 })
 
-test('the first note is about HTTP methods', async () => {
+test('a specific note is within the returned notes', async () => {
   const response = await api.get('/api/notes')
+
   // expect(response.body[0].content).toBe('HTML is easy')
   const contents = response.body.map(r => r.content)
   expect(contents).toContain(
     'Browser can execute only Javascript'
   )
+})
+
+// --------------------------新增post('/api/notes')------------------------
+test('a valid note can be added', async () => {
+  const newNote = {
+    content: 'async/await simplifies making async calls',
+    important: true,
+  }
+  await api
+    .post('/api/notes')
+    .send(newNote)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  /*  const response = await api.get('/api/notes')
+  const contents = response.body.map(r => r.content)
+  expect(response.body).toHaveLength(initialNotes.length + 1)*/
+  const notesAtEnd = await helper.notesInDb()
+  expect(notesAtEnd).toHaveLength(helper.initialNotes.length + 1)
+  const contents = notesAtEnd.map(n => n.content)
+  expect(contents).toContain(
+    'async/await simplifies making async calls'
+  )
+})
+
+test('note without content is not added', async () => {
+  const newNote = {
+    important: true
+  }
+  await api
+    .post('/api/notes')
+    .send(newNote)
+    .expect(400)
+
+  /*  const response = await api.get('/api/notes')
+  expect(response.body).toHaveLength(initialNotes.length)*/
+  const notesAtEnd = await helper.notesInDb()
+  expect(notesAtEnd).toHaveLength(helper.initialNotes.length)
+})
+
+// --------------------------查询单个笔记get(`/api/notes/${noteToView.id}`)------------------------
+test('a specific note can be viewed', async () => {
+  const notesAtStart = await helper.notesInDb()
+  const noteToView = notesAtStart[0]
+  const resultNote = await api
+    .get(`/api/notes/${noteToView.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const processedNoteToView = JSON.parse(JSON.stringify(noteToView))
+  expect(resultNote.body).toEqual(processedNoteToView)
+})
+
+// --------------------------删除笔记delete(`/api/notes/${noteToView.id}`)------------------------
+test('a note can be deleted', async () => {
+  const notesAtStart = await helper.notesInDb() // 查询数据库全部笔记
+  const noteToDelete = notesAtStart[0] // 删除第一个笔记
+  await api
+    .delete(`/api/notes/${noteToDelete.id}`)
+    .expect(204)
+
+  const notesAtEnd = await helper.notesInDb() // 查询删除第一个笔记之后的全部笔记
+  expect(notesAtEnd).toHaveLength(
+    helper.initialNotes.length - 1
+  )
+  const contents = notesAtEnd.map(r => r.content)
+  expect(contents).not.toContain(noteToDelete.content)
 })
 
 // 所有的测试都运行完毕，须关闭Mongoose使用的数据库连接
